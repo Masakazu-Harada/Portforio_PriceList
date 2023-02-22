7cacfea1a30c9ceeef9e4d338cbecdc349dc91a0
Masakazu_Harada <seungri.uhm@gmail.com>
2023-02-23 00:28:37 +0900

    saple.rbメモ書き

---
+++ sample.rb
@@ -0,0 +1,20 @@
+if product.original_goods == 'original'
+
+def original?
+  self.original_goods == 'original'
+end
+
+if product.is_original?
+
+current_supplier_idがあったら
+
+product = Product.find(n)
+
+belongs_to :supplier, forign_key: :current_supplier_id
+belongs_to :current_supplier, forign_key: 
+
+product.supplier.location
+product.supplier.closing_time
+
+product.current_supplier.location
+product.current_supplier.closing_time
