29f1a9fa626994b4b59de82a12b86c7a851f2e1a
Masakazu_Harada <seungri.uhm@gmail.com>
2023-02-25 19:06:50 +0900

    new,create,indexのフロントの土台まで

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
@@ -9,6 +10,18 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    product = Product.new(product_params)
+    product.save!
+    redirect_to products_url, notice: "「#{product.name}」をカタログへ追記しました。"
+  end
+
   def edit
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :code, :catalog_page_number, :spec, :is_original)
   end
 end
