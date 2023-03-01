87a0478d9ec17aff18ead1b181ef65eba0fdffde
t4traw <t4traw@gmail.com>
2023-03-01 18:33:33 +0900

    不必要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'departments/index'
-    get 'departments/show'
-    get 'departments/new'
-    get 'departments/edit'
     resources :users
     resources :departments
   end
