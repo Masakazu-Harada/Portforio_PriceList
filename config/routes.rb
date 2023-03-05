b703ef844545939ce29dc11e4c51513bac2fba1c
Masakazu_Harada <seungri.uhm@gmail.com>
2023-03-05 23:14:35 +0900

    ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'sessions#new'
+  get 'dashboards/index'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   namespace :admin do
     resources :users
     resources :departments
