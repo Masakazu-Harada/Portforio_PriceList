b3d6471b5b0fe5e88f4d31171b66f60c1f05244e
Masakazu_Harada <seungri.uhm@gmail.com>
2023-03-06 23:16:26 +0900

    rootパスの変更など

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to dashboards_index_url, notice: "ログインしました。"
+      redirect_to root_url, notice: "ログインしました。"
     else
       render :new
     end
