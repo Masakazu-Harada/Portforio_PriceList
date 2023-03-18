d6f109c4ecb98e7d7c36e0fe1d106adaa10b1273
Masakazu_Harada <seungri.uhm@gmail.com>
2023-03-18 11:16:53 +0900

    売価登録のリダイレクの際のフラッシュメッセージ追加

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -6,11 +6,14 @@ class PricesController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     params['price'].each do |price_record|
-      price = @product.prices.find_by(rank_id: price_record['rank_id'])
-      price.price = price_record['price']
-      price.save
+      @price = @product.prices.find_by(rank_id: price_record['rank_id'])
+      @price.price = price_record['price']
     end
-    redirect_to products_url
+    if @price.save
+    redirect_to products_url, notice: "「#{@product.name}」の売価をカタログへ登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
