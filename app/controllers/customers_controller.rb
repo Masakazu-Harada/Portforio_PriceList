f65becc44b7d0cf750f0e1bdb33eabad9fd3c5d9
Masakazu_Harada <seungri.uhm@gmail.com>
2023-04-02 09:51:41 +0900

    customerのラジオボタン実装中

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,4 +6,17 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    customer = Customer.new
+    customer.save!
+    redirect_to customers_url, notice: "「#{customer.name}」のレコードを追加しました。"
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :share)
+    params.require(:rank).permit(:name)
+  end
 end
