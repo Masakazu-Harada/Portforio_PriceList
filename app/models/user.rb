446e798477833a5f3bcaf86381a7e4d2183ace52
Masakazu_Harada <seungri.uhm@gmail.com>
2023-04-17 23:03:04 +0900

    Userの削除ボタンで詰まる

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   #社員モデル 一人の社員が部署を兼任しているので多 対 多で関連付けをする。affiliationモデルで関連づけ
   has_secure_password
   has_many :affiliations, dependent: :destroy
-  has_many :departments, through: :affiliations
+  has_many :departments, through: :affiliations, source: :department
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
