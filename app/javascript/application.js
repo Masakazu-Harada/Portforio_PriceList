2bf2d950dc1f3a315f0f6796afe06ec8e631f89a
Masakazu_Harada <seungri.uhm@gmail.com>
2023-04-17 22:16:06 +0900

    お客様ログインと社員用ログインの実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 import "@hotwired/turbo-rails"
 import "controllers"
-
 require("bootstrap")
+
+Rails.start()
+ActiveStorage.start()
