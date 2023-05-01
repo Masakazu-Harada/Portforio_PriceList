9b7c22377b0860ca05e248c588abc82dc8e75c26
Masakazu_Harada <seungri.uhm@gmail.com>
2023-05-01 17:28:37 +0900

    departmentモデルのCRUD機能完成

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,41 @@
 class DepartmentsController < ApplicationController
   def index
+    @departments = Department.all
   end
 
   def show
+    @department = Department.find(params[:id])
   end
 
   def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+    @department.save!
+    redirect_to departments_url, notice: "「#{@department.name}」のレコードを新規登録しました。"
   end
 
   def edit
+    @department = Department.find(params[:id])
+  end
+
+  def update
+    @department = Department.find(params[:id])
+    @department.update!(department_params)
+    redirect_to departments_url, notice: "「#{@department.name}」のレコードを更新しました。"
+  end
+
+  def destroy
+    department = Department.find(params[:id])
+    department.destroy
+    redirect_to departments_url, notice: "「#{department.name}」のレコードを削除しました。"
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:name)
   end
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,5 @@ class Department < ApplicationRecord
   #部署モデル 営業部・購買部・管理部・業務部の４部署をnameカラムを持つ 中間モデルのaffiliationモデルを通してuserモデルのレコードを取得する 多 対 多
   has_many :affiliations, dependent: :destroy
   has_many :users, through: :affiliations
+  validates :name, presence: true
 end
