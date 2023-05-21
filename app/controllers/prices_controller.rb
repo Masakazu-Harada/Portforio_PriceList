5db220b2017578ea8214ea13d3bf99cca2f2b8d5
Masakazu_Harada <seungri.uhm@gmail.com>
2023-05-21 23:47:20 +0900

    Pricesコントローラーの修正

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -14,33 +14,42 @@ class PricesController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
+    errors = []
     params[:price].each do |price_params|
       rank = Rank.find(price_params[:rank_id])
-      price = @product.prices.new(price: price_params[:price], rank: rank)
-      if price.save
-        redirect_to prices_path, notice: "「#{@product.name}」の売価をカタログへ登録しました。"
-      else
-        redirect_to prices_path, alert: "価格の登録に失敗しました。"
-        return
+      price = @product.prices.new(
+        price: price_params[:price], 
+        future_price: price_params[:future_price], 
+        price_increase_date: price_params[:price_increase_date],
+        rank: rank
+      )
+      unless price.save
+        errors << "#{rank.name}の価格の登録に失敗しました"
       end
     end
-  end
-
-  def edit
-    @product = Product.find(params[:product_id])
-    @price = @product.prices.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:product_id])
-    @price = @product.prices.find(params[:id])
-    if @price.update(price_params)
-      redirect_to prices_path, notice: "価格が更新されました。"
+    if erros.empty?
+      redirect_to prices_path, notice: "「#{@product.name}」の売価をカタログへ登録しました。"
     else
-      flash.now[:alert] = "価格の更新に失敗しました。"
-      render :edit
+      flash[:alert] = errors.join(", ")
+      redirect_to new_product_price_path(@product)
     end
   end
+
+  #def edit
+    #@product = Product.find(params[:product_id])
+    #@price = @product.prices.find(params[:id])
+  #end
+
+  #def update
+    #@product = Product.find(params[:product_id])
+    #@price = @product.prices.find(params[:id])
+    #if @price.update(price_params)
+      #redirect_to prices_path, notice: "価格が更新されました。"
+    #else
+      #flash.now[:alert] = "価格の更新に失敗しました。"
+      #render :edit
+    #end
+  #end
 
   def bulk_edit
     @product = Product.find(params[:product_id])
@@ -49,14 +58,19 @@ class PricesController < ApplicationController
 
   def bulk_update
     @product = Product.find(params[:product_id])
-    price_params = params.require(:product).permit(prices: [:id, :rank_id, :price])
+    price_params = params.require(:product).permit(prices: [:id, :rank_id, :price, :future_price, :price_increase_date])
     price_update_success = true
   
     price_params[:prices].each do |price_data|
       price = Price.find(price_data[:id])
-      unless price.update(price: price_data[:price])
+      unless price.update(
+        price: price_data[:price],
+        future_price: price_data[:future_price],
+        price_increase_date: price_data[:price_increase_date]
+      )
         price_update_success = false
-        flash.now[:alert] = "価格の更新に失敗しました。"
+        flash.now[:alert] = "#{price.rank.name}の価格の更新に失敗しました。"
+        @prices = Price.where(product_id: @product.id).order(rank_id: :asc) 
         render :bulk_edit
         return
       end
@@ -70,6 +84,8 @@ class PricesController < ApplicationController
   private
 
   def price_params
-    params.require(:price).permit(:price, :rank_id)
+    params.require(:price).map do |price|
+      price.permit(:price, :future_price, :price_increase_date, :rank_id)
+    end
   end
 end
