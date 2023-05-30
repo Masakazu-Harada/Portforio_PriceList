83c53502f87bfab8dd7f193d96d264209d2aa6c8
Masakazu_Harada <seungri.uhm@gmail.com>
2023-05-30 09:15:35 +0900

    CostIncreaseHistoryモデルにafter_saveのupdate_product_supplier_costメソッドを作成

--- app/models/cost_increase_history.rb
+++ app/models/cost_increase_history.rb
@@ -1,4 +1,17 @@
 class CostIncreaseHistory < ApplicationRecord
   belongs_to :product_supplier
   belongs_to :user
+
+  after_save :update_product_supplier_cost
+
+  private
+
+  def update_product_supplier_cost
+    #前提としてバックグラウンドジョブを毎日決まった時間に行うとして、ここで値上げ予定日が今日またはそれ以前であった場合、
+    #after_saveが発動し、update_product_supplier_cost実行され、ProductSupplierモデルにもある、
+    #current_costが更新され最新の仕入原価が反映される実装。
+    if cost_revision_date <= Date.today
+      product_supplier.update(current_cost: current_cost)
+    end
+  end
 end
